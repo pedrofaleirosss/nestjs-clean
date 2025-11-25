e21261691a8a96af2f58e09e5eeae5031a81a75c
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-11-25 20:57:53 -0300

    Controller de listagem de perguntas

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,13 +1,13 @@
 import { BadRequestException, PipeTransform } from "@nestjs/common";
-import { ZodError, ZodObject } from "zod";
+import { ZodError, ZodType } from "zod";
 import { fromZodError } from "zod-validation-error";
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodObject<any>) {}
+  constructor(private schema: ZodType) {}
 
   transform(value: unknown) {
     try {
-      this.schema.parse(value);
+      return this.schema.parse(value);
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({
