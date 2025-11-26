ce11b9f36d017ac0912b365ea3f67b872734fedb
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-11-26 20:33:52 -0300

    Controller: Buscar Pergunta por Slug

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -2,6 +2,7 @@ import { Either, left, right } from "@/core/either";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
+import { Injectable } from "@nestjs/common";
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string;
@@ -11,6 +12,8 @@ type GetQuestionBySlugUseCaseResponse = Either<
   ResourceNotFoundError,
   { question: Question }
 >;
+
+@Injectable()
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
