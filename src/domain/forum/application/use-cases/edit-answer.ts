f650d863e603022cc74a6494b49905b119ad48de
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-11-26 22:13:37 -0300

    Controller: Editar Resposta

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -7,6 +7,7 @@ import { AnswerAttachmentList } from "../../enterprise/entities/answer-attachmen
 import { AnswerAttachment } from "../../enterprise/entities/answer-attachment";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { AnswerAttachmentsRepository } from "../repositories/answer-attachments-repository";
+import { Injectable } from "@nestjs/common";
 
 interface EditAnswerUseCaseRequest {
   authorId: string;
@@ -19,6 +20,8 @@ type EditAnswerUseCaseResponse = Either<
   ResourceNotFoundError | NotAllowedError,
   { answer: Answer }
 >;
+
+@Injectable()
 export class EditAnswerUseCase {
   constructor(
     private answersRepository: AnswersRepository,
