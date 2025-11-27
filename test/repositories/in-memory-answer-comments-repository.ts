8720aff4f9b60f53dd5a0e3f6fa0a5f0a0e9ffaf
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-11-27 18:21:31 -0300

    Comentário da resposta com autor

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+import { InMemoryStudentsRepository } from "./in-memory-students-repository";
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author";
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = [];
+
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
 
   async findById(id: string) {
     const answerComment = this.items.find((item) => item.id.toString() === id);
@@ -23,6 +27,35 @@ export class InMemoryAnswerCommentsRepository
       .slice((page - 1) * 20, page * 20);
 
     return answerComment;
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams
+  ) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) =>
+          student.id.equals(comment.authorId)
+        );
+
+        if (!author) {
+          throw new Error(`Author with ID ${comment.authorId} not found.`);
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        });
+      });
+
+    return answerComments;
   }
 
   async create(answerComment: AnswerComment): Promise<void> {
